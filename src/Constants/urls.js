4e56c0998455feb37847c3d3354c11f558eb1fb6
T3NZ0R <t3nz000r@gmail.com>
2022-08-22 15:23:54 +0300

    Pet Project

---
+++ src/Constants/urls.js
@@ -0,0 +1,18 @@
+export const baseURL = 'https://api.themoviedb.org/3/';
+export const baseURL_img = 'https://image.tmdb.org/t/p/original';
+export const urls = {
+    nowPlaying: 'movie/now_playing',
+    popular: 'movie/popular',
+    topRated: 'movie/top_rated',
+    upcoming: 'movie/upcoming',
+    genre: 'genre/movie/list',
+    discover: 'discover/movie',
+    requestToken: 'authentication/token/new',
+    logInUser: 'authentication/token/validate_with_login',
+    search:'search/movie',
+    movieId:'movie/',
+    sessionId:'authentication/guest_session/new'
+};
+export const key = '?api_key=220b89d1e305e345a19d3fa0b6b75cec';
+
+
