968db25fbf48898e3130ac5ff49e0b360c8f6dfb
Vitalii Shtark <t3nz000r@gmail.com>
2022-11-16 15:05:37 +0200

    Pet Project predeploy

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -109,7 +109,7 @@ const Header = () => {
 
                     <Route path={'/about-us'} element={<AboutUs/>}/>
 
-                    <Route path={'*'} element={<NoInfo/>}/>
+                    {/*<Route path={'*'} element={<NoInfo/>}/>*/}
 
                 </Routes>
             </div>
